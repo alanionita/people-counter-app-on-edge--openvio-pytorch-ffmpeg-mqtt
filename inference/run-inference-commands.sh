b5cd578327c64d957c3a7f44b52a2c8ee25710d8
Alan Ionita <a.g.ionita@gmail.com>
2020-06-28 13:26:49 +0100

    FEAT : (docker) add new setup for inference setup in docker compose
    Also adds an executable bash script for running inference commands
    
    Also adds an env file in order to obfuscate the env variables used for inference

---
+++ inference/run-inference-commands.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# set model path
+set_model_path() {
+    if [ "$MODEL_PRECISION" ]; then
+        MODEL_PATH=$(find $MODEL_DIR -name "$MODEL_NAME.xml" | grep $MODEL_PRECISION)
+    else
+        MODEL_PATH=$(find $MODEL_DIR -name "$MODEL_NAME.xml")
+    fi
+
+    export MODEL_PATH=${MODEL_PATH%/*}
+}
+
+set_model_path
+
+python3 main.py -m $MODEL_PATH/$MODEL_NAME.xml
+
+# model-downloader --print_all
