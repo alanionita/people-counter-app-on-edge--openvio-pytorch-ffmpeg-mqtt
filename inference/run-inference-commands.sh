552b0e7ba62b620608473a23f0795cfa2268fd81
Alan Ionita <a.g.ionita@gmail.com>
2020-07-08 20:25:22 +0100

    FEAT : (inference) updates the inference service to use vehicle attributes model
    Cleaned up shell scripts
    
    Updates env variables

--- inference/run-inference-commands.sh
+++ inference/run-inference-commands.sh
@@ -14,6 +14,8 @@ set_model_path() {
 
 set_model_path
 
-python3 main.py -m $MODEL_PATH/$MODEL_NAME -i data/sitting-on-car.jpg
+# python3 main.py -m $MODEL_PATH/$MODEL_NAME -i data/sitting-on-car.jpg
+python3 main.py -m $MODEL_PATH/intel/vehicle-attributes-recognition-barrier-0039/FP32/vehicle-attributes-recognition-barrier-0039 -i data/cars.mp4
 
-# model-downloader --print_all
+exec "$@"
+# ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://localhost:3004/fac.ffm
