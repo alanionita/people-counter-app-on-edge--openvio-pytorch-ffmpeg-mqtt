2b860c8d6162a2e3049002e32aec70bb362190a6
Alan Ionita <a.g.ionita@gmail.com>
2020-07-25 13:44:54 +0100

    Merge branch 'master' of github.com:alanionita/people-counter-app-on-edge--openvio-pytorch-ffmpeg-mqtt

--- inference/run-inference-commands.sh
+++ inference/run-inference-commands.sh
@@ -4,6 +4,8 @@
 echo "source /opt/intel/openvino/bin/setupvars.sh" >>/root/.bashrc
 source /opt/intel/openvino/bin/setupvars.sh
 
-python3 main.py -m $MODEL_DIR/frozen_inference_graph -i data/cars.mp4 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://ffserver:3004/fac.ffm
+# python3 main.py -m $MODEL_DIR/frozen_inference_graph -i data/cars.mp4 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://ffserver:3004/fac.ffm
+
+python3 main.py -m $MODEL_DIR/frozen_inference_graph -i data/cars.mp4 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - video.mp4
 
 exec "$@"
