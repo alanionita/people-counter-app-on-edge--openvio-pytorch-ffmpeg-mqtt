4b470aa35720c7df29c26894dbaa635c0d2acd10
Alan Ionita <a.g.ionita@gmail.com>
2020-07-25 18:30:25 +0100

    BACKUP : saving some of the debugging attempts with ffmpeg

--- inference/run-inference-commands.sh
+++ inference/run-inference-commands.sh
@@ -4,8 +4,13 @@
 echo "source /opt/intel/openvino/bin/setupvars.sh" >>/root/.bashrc
 source /opt/intel/openvino/bin/setupvars.sh
 
-# python3 main.py -m $MODEL_DIR/frozen_inference_graph -i data/cars.mp4 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://ffserver:3004/fac.ffm
+# python3 main.py -m $MODEL_DIR/frozen_inference_graph -i data/cars.mp4 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://localhost:3004/fac.ffm
 
-python3 main.py -m $MODEL_DIR/frozen_inference_graph -i data/cars.mp4 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - video.mp4
+# python3 main.py -m $MODEL_DIR/frozen_inference_graph -i data/cars.mp4 | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - video.mp4
+
+# python3 main.py -m $MODEL_DIR/frozen_inference_graph -i data/cars.mp4 | ffmpeg -v warning -f v4l2 -vcodec mjpeg -r 10 -pixel_format bgr24 -s 640x480 -i - out/out.mp4
+
+
+python3 main.py -i data/cars.mp4 -m $MODEL_DIR/frozen_inference_graph | ffmpeg -v warning -f rawvideo -pixel_format bgr24 -video_size 768x432 -framerate 24 -i - http://localhost:3004/fac.ffm
 
 exec "$@"
